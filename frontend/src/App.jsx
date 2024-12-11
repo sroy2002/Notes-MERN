d86bcfe3d98acc863daf5311d6fd85a60cc785a7
Suranjana Roy <roysupra75@gmail.com>
2024-12-11 14:57:34 +0530

    ...

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Auth0Provider } from "@auth0/auth0-react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home.jsx";
 import Profile from "./Pages/Profile.jsx";
@@ -7,12 +8,20 @@ import "./Styles/App.scss";
 function App() {
   return (
     <div className="my-app">
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </BrowserRouter>
+      <Auth0Provider
+        domain="dev-2zvnt7b3vewhots2.us.auth0.com"
+        clientId="YOUR_CLIENT_ID"
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+        }}
+      >
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </BrowserRouter>
+      </Auth0Provider>
     </div>
   );
 }
